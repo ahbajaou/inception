664b4fc4f1f893390c5f38ab9e627bf24fdbd454
ahbajaou <ahbajaou@ahbajaou.ahbajaou>
2024-06-03 11:29:44 +0100

    inception

--- Makefile
+++ Makefile
@@ -8,9 +8,5 @@ re:
 	@docker-compose -f ./srcs/docker-compose.yml up --build
 
 clean:
-	docker stop $(docker ps) \
-	&& docker rm $(docker ps -aq) \
-	&& docker rmi $(docker images -aq) \
-	&& docker volume prune -af \
-	&& docker network prune -f \
-	&& docker system prune -af
+	docker system prune -af && docker volume prune --force
+	# docker rm $(docker images -a)
--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -2,6 +2,8 @@
 
 service mariadb start
 
+sleep 2
+
 mysql_secure_installation << EOF
 $root_pass
 n
@@ -16,4 +18,9 @@ mariadb -e "CREATE USER IF NOT EXISTS '${data_user}'@'%' IDENTIFIED BY '${data_p
 mariadb -e "GRANT ALL PRIVILEGES ON ${data_name}.* TO '${data_user}'@'%' ;"
 mariadb -e "FLUSH PRIVILEGES;"
 service mariadb stop
+
+old="bind-address            = 127.0.0.1"
+new="bind-address            = 0.0.0.0"
+sed -i "s/$old/$new/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
 mysqld_safe
